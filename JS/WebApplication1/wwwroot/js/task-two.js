1bf2cb392c8ab39484099273496457f412f1c776
Фигловский Дмитрий <d.figlovskiy_dev@qpdev.ru>
2021-03-18 14:43:25 +0300

    Task 2 Calculator done

--- JS/WebApplication1/wwwroot/js/task-two.js
+++ JS/WebApplication1/wwwroot/js/task-two.js
@@ -1,1 +1,56 @@
-﻿
+﻿function Calculator(num) {
+    if (this instanceof Calculator && !this.__previouslyConstructedByCalculator) {
+        this.__previouslyConstructedByCalculator = true;
+    }
+    else {
+        throw new Error('Use like constructor only!');
+    }
+
+    let priorityOperations = [];
+    let plusArray = [];
+    let minusArray = [];
+    let multiArray = [];
+    let divArray = [];
+
+    return {
+        plus: function (plusNum) {
+            plusArray.push(plusNum);
+            return this;
+        },
+        minus: function (minusNum) {
+            minusArray.push(minusNum);
+            return this;
+        },
+        multiply: function (multiNum) {
+            priorityOperations.push('*');
+            multiArray.push(multiNum);
+            return this;
+        },
+        divide: function (divNum) {
+            priorityOperations.push('/');
+            divArray.push(divNum);
+            return this;
+        },
+        calculate: function () {
+            let res = num;
+            priorityOperations.forEach((operation) => {
+                if (operation == '*') {
+                    res *= multiArray.shift();
+                }
+                if (operation == '/') {
+                    res /= divArray.shift();
+                }
+            });
+
+            plusArray.forEach((plusNum) => {
+                res += plusNum;
+            });
+
+            minusArray.forEach((minusNum) => {
+                res -= minusNum;
+            });
+
+            return res;
+        }
+    };
+}
